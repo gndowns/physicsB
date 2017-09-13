8eaa50e4779bd07f8da3afce3c3ddde1a7d9a16a
Gabriel Downs <gndowns@gmail.com>
2017-09-13 13:56:59 -0400

    animation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys, json
+from time import sleep
 
 class Cell:
   def __init__(self, on=0, d=0):
@@ -12,10 +13,28 @@ def print_space(space):
     print(c_out, end="")
   print()
 
+def time_step(space):
+  out = [None] * len(space)
+  for i in range(len(out)):
+    out[i] = Cell()
+  for i,c in enumerate(space):
+    if c.on:
+      dest = i + c.d
+      if dest > len(space): dest = dest-20
+      out[dest] = Cell(**c.__dict__)
+  return out
+
+def animate(space):
+  while(True):
+    print_space(space)
+    space = time_step(space)
+    sleep(1)
+    print("\033[21A\r")
+
 conf = json.load(open(sys.argv[1]))
 space = [None] * 400
 for i in range(len(space)):
   c = dict(conf.get(str(i)) or {})
   space[i] = Cell(**c)
 
-print_space(space)
+animate(space)
