8059f21a2dfb21227143942c098b64acfe201d21
Gabriel Downs <gndowns@gmail.com>
2017-09-14 01:08:36 -0400

    sort of working

--- main.py
+++ main.py
@@ -10,56 +10,27 @@ Colors = {
 }
 
 class Cell:
-  def __init__(self, on=0, dx=0, dy=0,
+  def __init__(self, x, y, dx=0, dy=0,
     color=None, color_code=None, nbhds=None):
-    self.on = on
+    self.x = x
+    self.y = y
     self.dx = dx
     self.dy = dy
     self.color = color
     self.color_code = color_code or Colors.get(color) or Colors['white']
     self.nbhds = nbhds if nbhds else []
 
-def time_step(space):
-  make_nbhds(space)
-  detect_collisions(space)
+def make_nbhds(particles, n):
+  nbhds = {}
+  for coords,p in particles.items():
+    i,j = [int(x) for x in coords.split(' ')]
+    x = str(max(min(i + p.dx, n - 1), 0))
+    y = str(max(min(j + p.dy, n - 1), 0))
+    key = ' '.join([x, y])
 
-  return fulfill_nbhds(space)
-
-def fulfill_nbhds(space):
-  out = [[Cell() for _ in range(20)] for _ in range(20)]
-  for i,l in enumerate(space):
-    for j,c in enumerate(l):
-      if c.on:
-        dest_x = max(min(i + c.dx, len(out) - 1), 0)
-        dest_y = max(min(j + c.dy, len(out) - 1), 0)
-        c.nbhds = []
-        out[dest_x][dest_y] = Cell(**c.__dict__)
-  return out
-
-
-def detect_collisions(space):
-  for i,l in enumerate(space):
-    for j,c in enumerate(l):
-      if len(c.nbhds) >= 2:
-        detect_collision(space, c.nbhds)
-
-def detect_collision(space, nbhds):
-  particles = [space[nbhds[i][0]][nbhds[i][1]] for i in range(len(nbhds))]
-  for p in particles:
-    p.dx = 0
-    p.dy = 0
-    for q in particles:
-      p.dx = p.dx + q.dx
-      p.dy = p.dy + q.dy
-
-
-def make_nbhds(space):
-  for i,l in enumerate(space):
-    for j,c in enumerate(l):
-      if c.on:
-        dest_x = max(min(i + c.dx, len(space) - 1), 0)
-        dest_y = max(min(j + c.dy, len(space) - 1), 0)
-        space[dest_x][dest_y].nbhds.append([dest_x, dest_y])
+    if not nbhds.get(key):
+      nbhds[key] = []
+    nbhds[key].append(coords)
 
 def flush_space(particles, n):
   for i in range(n):
@@ -67,8 +38,30 @@ def flush_space(particles, n):
       print(" - ", end="")
     print()
 
+def time_step(particles, n):
+  nbhds = make_nbhds(particles, n)
+  #  detect_collisions(space)
+
+  #  return fulfill_nbhds(space)
+  return True
+
+def move_cursor(x, d):
+  print('\033[{}{}'.format(x, d), end='')
+
+def publish_particles(particles):
+  for p in particles.values():
+    # set cursor
+    move_cursor(p.x + 1, "A")
+    move_cursor(p.y + 1, "C")
+    print(p.color_code + 'O', end='')
+    # reset cursor
+    move_cursor(p.x, "B")
+    move_cursor(p.y, "D")
+    print(Colors['white'])
+
 def animate(particles, n):
   flush_space(particles, n)
+  publish_particles(particles)
 
 conf = json.load(open(sys.argv[1]))
 particles = {}
